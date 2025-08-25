c11d78bdfee7c0346858153d62a81188b4493460
Hải Nguyễn Đình <haiholmes99@gmail.com>
2025-08-26 01:40:51 +0700

    [feature] build infra, build config server, discovery server, customer service

---
+++ customer/src/test/java/org/jerry/ecommerce/CustomerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.jerry.ecommerce;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CustomerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
